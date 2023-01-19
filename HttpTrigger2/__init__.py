fbb7b8282fcd7514ffaae81dbb8e4c06809b8ef1
Vincent <theVincent.Marzo@gmail.com>
2023-01-19 17:20:33 +0100

    removed arg 'connection_string' in functions

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -20,11 +20,11 @@ def recommend_old(user_id_str, n):
     return user_recs
 
 
-
-def get_recs_file(connection_string):
+def get_recs_file():
+    #def get_recs_file(connection_string):
     container_name = 'data-blob'
     blob_name = 'recs_idx_20_test.npy'
-    #connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=GGR/fUyGsTQb3m2wC6qnTQuc/BGok7FkGVP7gVe9CsVyGzlFMQyhI9WLMwUenCne3FbERfBa1C5M+AStdowY0g==;EndpointSuffix=core.windows.net"
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=GGR/fUyGsTQb3m2wC6qnTQuc/BGok7FkGVP7gVe9CsVyGzlFMQyhI9WLMwUenCne3FbERfBa1C5M+AStdowY0g==;EndpointSuffix=core.windows.net"
 
     blob = BlobClient.from_connection_string(
         conn_str=connection_string, 
@@ -41,12 +41,14 @@ def get_recs_file(connection_string):
 
 
 '''
-def compute_recs(connection_string):
+def compute_recs(:
+    def compute_recs(connection_string):
 
 '''
 
 
-def recommend(user_id_str, n, connection_string):
+def recommend(user_id_str, n):
+    #def recommend(user_id_str, n, connection_string):
     user_id = int(user_id_str)
 
     #TODO get last seen article from user
@@ -58,7 +60,8 @@ def recommend(user_id_str, n, connection_string):
         pass
 
     #get recommendations file from blob
-    recs_from_file = get_recs_file(connection_string)
+    recs_from_file = get_recs_file()
+    #recs_from_file = get_recs_file(connection_string)
 
     try:
         user_recs = recs_from_file[article_id,:n]
@@ -79,7 +82,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     key_word = 'userID'
     dict_key = 'key'
     #connection_string_param = 'connection_string'
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=GGR/fUyGsTQb3m2wC6qnTQuc/BGok7FkGVP7gVe9CsVyGzlFMQyhI9WLMwUenCne3FbERfBa1C5M+AStdowY0g==;EndpointSuffix=core.windows.net"
+    #connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=GGR/fUyGsTQb3m2wC6qnTQuc/BGok7FkGVP7gVe9CsVyGzlFMQyhI9WLMwUenCne3FbERfBa1C5M+AStdowY0g==;EndpointSuffix=core.windows.net"
 
     #works but connection string is on API
     user_id = req.params.get(key_word)
@@ -116,8 +119,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # get "n" recommendations and respond with a string
     n = 5
     if user_id:
-        #user_recs = recommend(user_id, n)
-        user_recs = recommend(user_id, n, connection_string)
+        user_recs = recommend(user_id, n)
+        #user_recs = recommend(user_id, n, connection_string)
         res = {'user_id': user_id, 'user_recs': user_recs}
         return func.HttpResponse(str(res))
     else:
