bcf1721898747011ebf9109deb7497391b849748
Vincent <theVincent.Marzo@gmail.com>
2023-01-20 10:06:34 +0100

    new branch

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -21,10 +21,10 @@ def recommend_old(user_id_str, n):
 
 
 
-def get_recs_file():
+def get_recs_file(connection_string):
     container_name = 'data-blob'
     blob_name = 'recs_idx_20_test.npy'
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=e5o6Ta6bAELTG23mpWue6ssJ/RfqSLmnYtOf/lDPRPE9r2bfwAqgQYopUf6wc3drAarUz8RJZDO3+AStCuZB6A==;EndpointSuffix=core.windows.net"
+    #connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=e5o6Ta6bAELTG23mpWue6ssJ/RfqSLmnYtOf/lDPRPE9r2bfwAqgQYopUf6wc3drAarUz8RJZDO3+AStCuZB6A==;EndpointSuffix=core.windows.net"
 
     blob = BlobClient.from_connection_string(
         conn_str=connection_string, 
@@ -41,7 +41,7 @@ def get_recs_file():
 
 
 
-def recommend(user_id_str, n):
+def recommend(user_id_str, n, connection_string):
     user_id = int(user_id_str)
 
     #TODO get last seen article form user
@@ -53,7 +53,7 @@ def recommend(user_id_str, n):
         pass
 
     #get recommendations file from blob
-    recs = get_recs_file()
+    recs = get_recs_file(connection_string)
 
     try:
         user_recs = recs[article_id,:n]
@@ -76,6 +76,8 @@ def recommend(user_id_str, n):
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=e5o6Ta6bAELTG23mpWue6ssJ/RfqSLmnYtOf/lDPRPE9r2bfwAqgQYopUf6wc3drAarUz8RJZDO3+AStCuZB6A==;EndpointSuffix=core.windows.net"
+
     # get user_id
     key_word = 'userID'
     user_id = req.params.get(key_word)
@@ -90,7 +92,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # get "n" recommendations and respond with a string
     n = 5
     if user_id:
-        user_recs = recommend(user_id, n)
+        user_recs = recommend(user_id, n, connection_string)
         res = {'user_id': user_id, 'user_recs': user_recs}
         return func.HttpResponse(str(res))
     else:
