beaacd9ba24eb6b68a3d7a0c02a16c2458a6b404
Vincent <theVincent.Marzo@gmail.com>
2023-01-20 09:55:31 +0100

    put back commented code

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -19,6 +19,7 @@ def recommend_old(user_id_str, n):
     return user_recs
 
 
+#def get_recs_file(connection_string):
 def get_recs_file():
     container_name = 'data-blob'
     blob_name = 'recs_idx_20.npy'
@@ -45,6 +46,7 @@ def compute_recs(connection_string):
 
 
 
+#def recommend(user_id_str, n, connection_string):
 def recommend(user_id_str, n):
     user_id = int(user_id_str)
 
@@ -58,6 +60,7 @@ def recommend(user_id_str, n):
 
     #get recommendations file from blob
     recs_from_file = get_recs_file()
+    #recs_from_file = get_recs_file(connection_string)
 
     try:
         user_recs = recs_from_file[article_id,:n]
@@ -76,7 +79,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # get user_id
     key_word = 'userID'
-
+    #dict_key = 'key'
+    #connection_string_param = 'connection_string'
+    #connection_string = "DefaultEndpointsProtocol=https;AccountName=oc9serverlessgroup87ea;AccountKey=GGR/fUyGsTQb3m2wC6qnTQuc/BGok7FkGVP7gVe9CsVyGzlFMQyhI9WLMwUenCne3FbERfBa1C5M+AStdowY0g==;EndpointSuffix=core.windows.net"
+    
     #works but connection string is on API
     user_id = req.params.get(key_word)
     if not user_id:
@@ -87,10 +93,33 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             user_id = req_body.get(key_word)
 
+    '''    
+    user_id = req.params.get(key_word)
+    connection_string = req.params.get(connection_string_param)
+    if not user_id:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            user_id, connection_string = req_body.get(key_word)
+            connection_string = req.params.get(connection_string_param)
+    '''
+    '''
+    req = req.params.get(dict_key)
+    user_id, connection_string = ' '.split(req)
+    if not user_id:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+    '''
+
     # get "n" recommendations and respond with a string
     n = 5
     if user_id:
         user_recs = recommend(user_id, n)
+        #user_recs = recommend(user_id, n, connection_string)
         res = {'user_id': user_id, 'user_recs': user_recs}
         return func.HttpResponse(str(res))
     else:
