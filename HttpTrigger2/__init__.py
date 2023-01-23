a90811ddefbe136c4bc5c5850fd14e5f6af20d0e
Vincent <theVincent.Marzo@gmail.com>
2023-01-23 14:19:24 +0100

    computing top n instead of reading from file

--- HttpTrigger2/__init__.py
+++ HttpTrigger2/__init__.py
@@ -4,6 +4,7 @@ from io import BytesIO
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 import azure.functions as func
 from azure.storage.blob import BlobClient
 
@@ -17,43 +18,62 @@ def get_user_read_list(user_id, time_click_df):
 
 
 
-def recommend(user_id_str, n, timeClick, recsFile):
+def extract_with_indices(x, idx):
+    return x[np.arange(x.shape[0])[:, None], idx]
+
+
+
+def compute_top_n(idx, n, embeddingsFile):
+    embeddings = np.load(BytesIO(embeddingsFile.read()))
+
+    # compute cosine similarities
+    embedding_rows = embeddings[idx,:].reshape(1, -1)
+    embeddings_without_i = np.delete(embeddings, idx, axis=0)
+    res = cosine_similarity(embedding_rows, embeddings_without_i)
+
+    # get top "n" similarities
+    top_n_indices = np.argpartition(res,-n)[:,-n:]
+    top_n_values = extract_with_indices(res, top_n_indices)
+    top_n_indices = extract_with_indices(top_n_indices, np.flip(np.argsort(top_n_values), axis=1))
+
+    return res
+
+
+
+def recommend(user_id_str, n, timeClick, recsFile, embeddingsFile):
     user_id = int(user_id_str)
-
-    #TODO get last seen article form user
-    #article_id = user_id  # temp
-    time_click = time_click = pd.read_pickle(BytesIO(timeClick.read()))
-    read_list = get_user_read_list(user_id, time_click)
-    article_id = read_list[0]
-    '''
+    
     try:
-       #temp
-    except:
-        # new user, no article read
+        # get last seen article form user
+        time_click = time_click = pd.read_pickle(BytesIO(timeClick.read()))
+        read_list = get_user_read_list(user_id, time_click)
+        article_id = read_list[0]
+    except IndexError:
+        # new user, no article read:
         #TODO use cold start
-    '''
-
-    #get recommendations file from blob
-    recs = np.load(BytesIO(recsFile.read()))
-
-    user_recs = recs[article_id,:n]
-    user_recs = list(user_recs)
+        pass
+    
     '''
     try:
-        #temp
-    except:
-        # new article: not in recs file
-        #TODO need to compute cosine similarites
-        pass
+        # get recommendations file from blob
+        recs = np.load(BytesIO(recsFile.read()))
+        user_recs = recs[article_id,:n]
+        user_recs = list(user_recs)
+    except IndexError:
+        # new article, not in recs file:
+        # compute cosine similarites
+        user_recs = compute_top_n(article_id, n, embeddingsFile)
     '''
+    #temp
+    user_recs = compute_top_n(article_id, n, embeddingsFile)
 
     return user_recs
-    
+
 
 
 def main(req: func.HttpRequest, 
          recsFile: func.InputStream, 
-         embeddings: func.InputStream, 
+         embeddingsFile: func.InputStream, 
          timeClick: func.InputStream) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
@@ -71,7 +91,7 @@ def main(req: func.HttpRequest,
     # get "n" recommendations and respond with a string
     n = 5
     if user_id:
-        user_recs = recommend(user_id, n, timeClick, recsFile)
+        user_recs = recommend(user_id, n, timeClick, recsFile, embeddingsFile)
 
         res = {'user_id': user_id, 'user_recs': user_recs}
         return func.HttpResponse(str(res))
